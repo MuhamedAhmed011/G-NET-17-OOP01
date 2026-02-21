86cb257f9d268217dc6424276f0724bb81f4da99
MuhamedAhmed011 <aklmuhamedahmed@gmail.com>
2026-02-21 20:50:59 +0200

    Question01

--- OOPApp1/Program.cs
+++ OOPApp1/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace OOPApp1
+﻿using System;
+
+namespace OOPApp1
 {
     internal class Program
     {
+
+        //class Person
+        //{
+        //    public string Name;
+        //}
+        //struct Person
+        //{
+        //    public string Name;
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Part 01
+
+            #region Question01
+            ////class → reference type → stored in heap → reference copy → supports inheritance
+            ////struct → value type → stored on stack → value copy → no inheritance
+            //Person p1 = new Person();
+            //p1.Name = "Ahmed";
+
+            //Person p2 = p1;   
+            //p2.Name = "Mohamed";
+
+            ////// if class
+            ////Console.WriteLine(p1.Name); // Mohamed
+            ////Console.WriteLine(p2.Name); // Mohamed
+
+            //// if struct
+            //Console.WriteLine(p1.Name); // Ahmed
+            //Console.WriteLine(p2.Name); // Mohamed 
+            #endregion
+
         }
+    
     }
 }
